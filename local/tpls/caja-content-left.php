b57661d5bdc9205b1a4674b52215e4fa7982315e
Florencia <florencia.alvado@gmail.com>
2018-03-12 10:29:37 -0300

    Update caja-content-left.php

--- local/tpls/caja-content-left.php
+++ local/tpls/caja-content-left.php
@@ -30,16 +30,7 @@ $menu = $app->getMenu();
 <div class="t3-wrapper"> <!-- Need this wrapper for off-canvas menu. Remove if you don't use of-canvas -->
 <div class="container main-wrapper">
 
-<?php if ($this->params->get('headertype')=="acm"): ?>
-  <?php $this->loadBlock('header-h') ?>
-  <?php endif ?>
-  <?php if ($this->params->get('headertype')=="T3-normal"): ?>
-  <?php $this->loadBlock('header-default-boxed') ?>
-  <?php $this->loadBlock('mainnav-boxed') ?>
-    <?php endif;?>
-  <?php if ($this->params->get('headertype')=="sticky"): ?>
-  <?php //echo 'Sticky solo en full width'; ?>
-  
+
    <?php $this->loadBlock('header-default-boxed') ?>
   <?php $this->loadBlock('mainnav-boxed') ?>
     <?php endif;?>
