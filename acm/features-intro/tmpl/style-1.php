220df02a9369ecbad802ccb0bb6fe1356a2af149
AFlorencia <florencia.alvado@gmail.com>
2018-07-16 12:23:51 -0300

    Update feat intro
    Change from fa icons to svg sprites

--- acm/features-intro/tmpl/style-1.php
+++ acm/features-intro/tmpl/style-1.php
@@ -2,7 +2,11 @@
 	$featuresImg 				= $helper->get('block-bg');
 	$fullWidth = $helper->get('full-width');
 	$featuresBackground  = 'background-image: url("'.$featuresImg.'"); background-repeat: no-repeat; background-size: cover; background-position: center center;';
+	$count      = $helper->getRows('data.title');
+	$col        = $helper->get('columns');
 ?>
+
+
 <div class="section-inner <?php echo $helper->get('block-extra-class'); ?>" <?php if($featuresImg): echo 'style="'.$featuresBackground.'"'; endif; ?>>
 
 	<div class="acm-features <?php echo $helper->get('features-style'); ?> style-1">
@@ -11,40 +15,35 @@
 				<h2 class="features-description"><?php echo $helper->get('features-description'); ?></h2>
 			<?php endif ; ?>
 			
-			<div class="<?php if(!$fullWidth): ?> row <?php else: ?> clearfix <?php endif; ?> items-container">
-			<?php $count = $helper->getRows('data.title'); ?>
-			<?php $column = 2; ?>
-			<?php for ($i=0; $i<$count; $i++) : ?>
-			<?php 
-		//	echo $count%$i;
-			if ($i%3==0) //echo '<div class="col-md-6">';
-			 ?>
-			<div class="serv-item">
-					<div class="item-wrap">
-						<?php if($helper->get('data.title', $i)) : ?>
+			<div class="<?php if(!$fullWidth): ?> row <?php else: ?> clearfix <?php endif; ?> ">
+			<?php
+			
+			for ($i=0; $i < $count; $i++) :
+        //  if ($i%$col==0) echo '<div class="row">'; 
+				?>
+			
+				<div class="features-item col-sm-4">
+					<div class="f-item">
+					<?php if($helper->get('data.title', $i)) : ?>
 						<h3><?php echo $helper->get('data.title', $i) ?></h3>
 					<?php endif ; ?>
-					<div class="item-content clearfix">
 					<?php if($helper->get('data.font-icon', $i)) : ?>
-					<div class="svg-icon">
+						<div class="font-icon">
 						<svg class="icon">
 <use xlink:href="images/sprites.svg#<?php echo $helper->get('data.font-icon', $i) ?>" />
 </svg>
 						</div>
 					<?php endif ; ?>
 	
+			
 					
 					
-				
 					
 					<?php if($helper->get('data.description', $i)) : ?>
-						<?php echo $helper->get('data.description', $i) ?>
+						<p><?php echo $helper->get('data.description', $i) ?></p>
 					<?php endif ; ?>
-</div>
-					</div>
-				</div>
-				<?php  ?>
-			<?php	if ($i%3==2) //echo '</div>'; ?>
+				</div></div>
+				<?php // if ( ($i%$col==($col-1)) || $i==($count-1) )  echo '</div>'; ?>
 			<?php endfor ?>
 			</div>
 		<?php if(!$fullWidth): ?></div><?php endif; ?>
