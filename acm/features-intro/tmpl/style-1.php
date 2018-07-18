4102c534754eed03585ac1176611739aac107808
AFlorencia <florencia.alvado@gmail.com>
2018-07-18 13:28:23 -0300

    Update ACM
    feat Intro style 1

--- acm/features-intro/tmpl/style-1.php
+++ acm/features-intro/tmpl/style-1.php
@@ -9,15 +9,15 @@
 	?>
 
 
-<div class="section-inner <?php echo $helper->get('block-extra-class'); ?>" <?php if($featuresImg): echo 'style="'.$featuresBackground.'"'; endif; ?>>
+
 
 	<div class="acm-features <?php echo $helper->get('features-style'); ?> style-1">
-		<?php if(!$fullWidth): ?><div class="container"><?php endif; ?>
+		
 			<?php if($helper->get('features-description')) : ?>
 				<h2 class="features-description"><?php echo $helper->get('features-description'); ?></h2>
 			<?php endif ; ?>
 			
-			<div class="<?php if(!$fullWidth): ?> row <?php else: ?> clearfix <?php endif; ?> ">
+		
 			<?php
 
 			
@@ -28,13 +28,14 @@
 				<div class="features-item col-sm-<?php echo $colzise; ?>">
 					<div class="f-item">
 					<?php if($helper->get('data.title', $i)) : ?>
-						<h3><?php echo $helper->get('data.title', $i) ?></h3>
+						<h3><a href="<?php echo $helper->get('data.link', $i) ?>"><?php echo $helper->get('data.title', $i) ?></a></h3>
 					<?php endif ; ?>
 					<?php if($helper->get('data.font-icon', $i)) : ?>
 						<div class="font-icon">
+							<a href="<?php echo $helper->get('data.link', $i) ?>">
 						<svg class="icon">
 <use xlink:href="images/sprites.svg#<?php echo $helper->get('data.font-icon', $i) ?>" />
-</svg>
+</svg></a>
 						</div>
 					<?php endif ; ?>
 	
@@ -48,7 +49,6 @@
 				</div></div>
 				<?php  if ( ($i%$col==($col-1)) || $i==($count-1) )  echo '</div>'; ?>
 			<?php endfor ?>
-			</div>
-		<?php if(!$fullWidth): ?></div><?php endif; ?>
+			
+		
 	</div>
-</div>
