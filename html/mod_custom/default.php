febf521b0971cd24d93b8b7b33a2c30070d512f2
AFlorencia <florencia.alvado@gmail.com>
2018-08-27 14:17:34 -0300

    Update maaany fileees
    *all

--- html/mod_custom/default.php
+++ html/mod_custom/default.php
@@ -16,11 +16,14 @@ $classe ='img';
 ?>
 
 
-<div class="custom<?php echo $moduleclass_sfx ?> <?php echo $params->get('module-bg-color'); ?>" <?php if ($params->get('backgroundimage')) : ?> style="background-image:url(<?php echo $params->get('backgroundimage');?>)"<?php endif;?> >
+<div class="custom <?php echo $moduleclass_sfx ?> <?php echo $params->get('module-bg-color'); ?>" <?php if ($params->get('backgroundimage')) : ?> style="background-image:url(<?php echo $params->get('backgroundimage');?>)"<?php endif;?> >
 	<?php echo $module->content;?>
    <?php if ($params->get('showimg')=='1') { ?>
-   
-   <img src="https://placeholdit.imgix.net/~text?txtsize=<?php echo $params->get('txtsize');?>&bg=<?php echo substr($params->get('bgcolor'),1);?>&txtclr=<?php echo substr($params->get('txtcolor'),1);?>&txt=<?php echo $params->get('txt');?>&w=<?php echo $params->get('width');?>&h=<?php echo $params->get('height');?>&txttrack=0" class="<?php echo $classe; ?>"  style="padding:15px 0;"/>
+  
+  
+ 
+
+   <img src="https://via.placeholder.com/<?php echo $params->get('width');?>/<?php echo substr($params->get('bgcolor'),1);?>/<?php echo substr($params->get('textcolor'),1);?>?text=<?php echo $params->get('txt');?>" class="img-responsive"  style="padding:15px 0;"/>
    
    <?php  } ?>
     
