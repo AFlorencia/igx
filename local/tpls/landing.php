c6dfd61e5ff1502988b05463b053c494ab9daff6
Florencia <florencia.alvado@gmail.com>
2019-01-15 13:46:04 -0300

    Update landing.php

--- local/tpls/landing.php
+++ local/tpls/landing.php
@@ -13,28 +13,21 @@
 */
 /* Landing page 
 
-
-
 */
-
 defined('_JEXEC') or die;
 $app = JFactory::getApplication();
 $menu = $app->getMenu();
 $anchorclass = $this->params->get('Anchor class');
-
 ?>
-
 <!DOCTYPE html>
 <html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" class='<jdoc:include type="pageclass" />'>
-
 <head>
-<jdoc:include type="head" />
-<?php $this->loadBlock('head') ?>
+    <jdoc:include type="head" />
+    <?php $this->loadBlock('head') ?>
 
-
- <?php  if($this->params->get('preload')==1){ ?>
- <style>
-div#preloader {
+    <?php  if($this->params->get('preload')==1){ ?>
+    <style>
+        div#preloader {
     width: 100%;
     height: 100vh;
     display: flex;
@@ -48,7 +41,6 @@ div#preloader {
     top: 0;
 }
 .svg-path{
-
 }
 @media(max-width: 480px){
     #preloader svg{
@@ -60,12 +52,10 @@ div#preloader {
 .preloadme {
   
 }
-
 body.loading {
     padding: 0;
     overflow: hidden;
 }
-
 .svg-icon {
 	-webkit-animation: rotate-center 1.2s linear infinite both;
 	        animation: rotate-center 1.2s linear infinite both;
@@ -90,168 +80,91 @@ body.loading {
             transform: rotate(360deg);
   }
 }
-
 <?php
  if($this->params->get('preloadcss')==1){ 
 //echo '<style>';
 echo $this->params->get('preloadcss');
 //echo '</style>';
- }
- ?>
+    }
+    ?>
 
-
-</style>
-
-<? } ?>
-
+    </style>
+    <? } ?>
 
 </head>
-
 <body data-spy="scroll" data-target=".navbar" class="loading">
+    <?php  if ($this->params->get('preload')==1){ ?>
+    <div id="preloader">
+        <?php $this->loadBlock('preload'); ?>
+    </div>
+    <?php } ?>
+    <div class="t3-wrapper">
+        <!-- Need this wrapper for off-canvas menu. Remove if you don't use of-canvas -->
 
- <?php  if ($this->params->get('preload')==1){ ?>
-<div id="preloader">
-  <?php $this->loadBlock('preload'); ?>
-</div>
- <?php } ?>
+        <?php $this->loadBlock('mainnav-land') ?>
+        <?php $this->loadBlock('spot-top') ?>
+        <?php $this->loadBlock('navhelper') ?>
+        <?php $this->loadBlock('spot-bottom') ?>
+        <?php	$this->loadBlock('mainbody-content-left');?>
+        <?php $this->loadBlock('footer') ?>
 
-<div class="t3-wrapper"> <!-- Need this wrapper for off-canvas menu. Remove if you don't use of-canvas -->
-
-
-<?php $this->loadBlock('mainnav-land') ?>
- 
-<?php $this->loadBlock('spot-top') ?>
-<?php $this->loadBlock('navhelper') ?>
-<?php $this->loadBlock('spot-bottom') ?>
-<?php	$this->loadBlock('mainbody-content-left');?>
-
-<?php $this->loadBlock('footer') ?>
-
-
-</div>
-<?php
+    </div>
+    <?php
 $delay = $this->params->get('preloadtime') - 100;
 if ($this->params->get('preload')==="1"){ 
   
   
   ?>
-<script type="text/javascript">
-jQuery(window).on('load', function() { // makes sure the whole site is loaded 
-  
-  jQuery('#preloader').delay(<?php echo $delay; ?>).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-  jQuery('body').delay(<?php echo $this->params->get('preloadtime'); ?>).css({'overflow':'visible'});
- 
-
-
-  jQuery('.preloadme').fadeIn();
-  jQuery('body').removeClass('loading');
- 
-});
-</script>
-<?php } ?>
-
- <?php  if ($this->params->get('back-to-top')==="1") { ?>
-<div id="back-to-top" data-spy="affix" data-offset-top="300" class="back-to-top hidden-xs hidden-sm affix-top"> 
-  <button class="btn btn-primary" title="Ir Arriba"><i class="fa fa-angle-up"></i></button> 
-</div>
-</div>
-
-<script type="text/javascript"> 
- (function($) { 
- // Back to top
- $('#back-to-top').on('click', function(){
-	 $('html, body').animate({scrollTop: 0}, 500);
-	 return false;
- });
-
-})(jQuery);
- 
-</script> 
-<?php  } ?>
-
-
-
-<script>
-
-
-(function ($) {	
-
-
-$('.nav li').click(function() {
-	
-	$(this).siblings('li').removeClass('active'); 
-	$(this).addClass('active'); });
-
-$(document).on('click', '.t3-mainnav a, .f-item a', function(event) {
-          var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-		}, 1250, 'easeInOutExpo');
-
-       // event.preventDefault();
-    });	
-	
-	
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
-            $('.navbar-toggle:visible').click();
-    });
-
-
-
-
-})(jQuery);
-
-AOS.init();
-
-</script>
-<script type="text/javascript">
-
-
-  
-
-
-jQuery('#planes').waypoint(function() {
-	  
-	       setTimeout(function(){
-
-        jQuery('.progress .progress-bar').each(function() {
-            var me = jQuery(this);
-            var perc = me.attr("data-percentage");
-
-            //TODO: left and right text handling
-
-            var current_perc = 0;
-
-            var progress = setInterval(function() {
-                if (current_perc>=perc) {
-                    clearInterval(progress);
-                } else {
-                    current_perc +=1;
-                    me.css('width', (current_perc)+'%');
-                }
-
-              //  me.text((current_perc)+'%');
-
-            }, 10);
-
+    <script type="text/javascript">
+    jQuery(window).on('load', function() { // makes sure the whole site is loaded 
+        jQuery('#preloader').delay(<?php echo $delay; ?>).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+        jQuery('body').delay(<?php echo $this->params->get('preloadtime'); ?>).css({
+            'overflow': 'visible'
         });
 
-    },200);
-	jQuery(this).css('color','red');
-	//console.log('focused');
-	  
-  });
+        jQuery('.preloadme').fadeIn();
+        jQuery('body').removeClass('loading');
+    });
+    </script>
+    <?php } ?>
+    <?php  if ($this->params->get('back-to-top')==="1") { ?>
+    <div id="back-to-top" data-spy="affix" data-offset-top="300" class="back-to-top hidden-xs hidden-sm affix-top">
+        <button class="btn btn-primary" title="Ir Arriba"><i class="fa fa-angle-up"></i></button>
+    </div>
+    </div>
+    <script type="text/javascript">
+    (function($) {
+        // Back to top
+        $('#back-to-top').on('click', function() {
+            $('html, body').animate({
+                scrollTop: 0
+            }, 500);
+            return false;
+        });
+    })(jQuery);
+    </script>
+    <?php  } ?>
 
-	 
-	   jQuery('.progress .progress-bar').each(function() {
-		     var me = jQuery(this);
-		   me.css('width', 0+'%');
-	   
-	   });
- 
+    <script>
+    (function($) {
 
-</script>
+        $('.nav li').click(function() {
+            $(this).siblings('li').removeClass('active');
+            $(this).addClass('active');
+        });
+        $(document).on('click', '.t3-mainnav a, .f-item a', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            }, 1250, 'easeInOutExpo');
+            // event.preventDefault();
+        });
 
+        // Closes the Responsive Menu on Menu Item Click
+        $('.navbar-collapse ul li a').click(function() {
+            $('.navbar-toggle:visible').click();
+        });
+    })(jQuery);
+    </script>
 </body>
 </html>
