b2378c34d2c7bc7b3f1af449a8e54efcfec39d0b
Florencia <florencia.alvado@gmail.com>
2019-01-17 14:03:51 -0300

    Update animated-flex.php

--- acm/features-intro/tmpl/animated-flex.php
+++ acm/features-intro/tmpl/animated-flex.php
@@ -7,16 +7,9 @@ $col        = $helper->get('columns');
 
 $aos ='';
 if($helper->get('animate')==1){
-$aos = ' data-aos="'.$helper->get('animations').'" data-aos-easing="'.$helper->get('easing').'"';
+        $aos = ' data-aos="'.$helper->get('animations').'" data-aos-easing="'.$helper->get('easing').'"';
 }
-?>
-<div class="acm-features <?php echo $helper->get('features-style'); ?> style-1">
 
-<?php if($helper->get('features-description')) : ?>
-<h2 class="features-description"><?php echo $helper->get('features-description'); ?></h2>
-<?php endif ; ?>
-<div class="flex-row">
-<?php
 $doc = JFactory::getDocument();
 // Add styles
 $gridGap = "";
@@ -27,16 +20,14 @@ $gap =$helper->get('gap');
 $gridGap = $gap * $cols;
 $colWidth = (1140 - $gridGap) / $cols;
 $style = '.flex-row {'.
-         'grid-template-columns: repeat(auto-fill, minmax('.$colWidth.'px, 1fr));'.
-         'grid-gap: '.$gap.'px;'.
-           '}'; 
+'grid-template-columns: repeat(auto-fill, minmax('.$colWidth.'px, 1fr));'.
+'grid-gap: '.$gap.'px;'.
+'}'; 
 $doc->addStyleDeclaration($style);
 ?>
-<?php
 
-for ($i=0; $i < $count; $i++) :
-
-?>
+<div class="flex-row">
+<?php for ($i=0; $i < $count; $i++) : ?>
 
 <div class="features-item"<?php echo $aos; ?>>
 
@@ -50,13 +41,12 @@ for ($i=0; $i < $count; $i++) :
 </h3>
 <?php endif ; ?>
 
-
 <?php if($helper->get('data.img-icon', $i)) : ?>
-						<div class="img-icon">
-							<img src="<?php echo $helper->get('data.img-icon', $i) ?>" alt="" />
-						</div>
-                    <?php endif ; ?>
-                    
+<div class="img-icon">
+<img src="<?php echo $helper->get('data.img-icon', $i) ?>" alt="" />
+</div>
+<?php endif ; ?>
+
 <?php if($helper->get('data.font-icon', $i)) : ?>
 <div class="font-icon">
 <?php if($helper->get('data.link', $i))	{ ?><a href="<?php echo $helper->get('data.link', $i) ?>">
@@ -76,8 +66,8 @@ for ($i=0; $i < $count; $i++) :
 <?php if($helper->get('data.description', $i)) : ?>
 <p><?php echo $helper->get('data.description', $i) ?></p>
 <?php endif ; ?>
+
 </div>
-<?php  if ( ($i%$col==($col-1)) || $i==($count-1) )  echo '</div>'; ?>
 <?php endfor ?>
 </div>
-</div>
+
